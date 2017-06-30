61e0ebb044627c1a02662e0ba61b6aa33e0832c2
afeiship <1290657123@qq.com>
2017-06-30 18:48:24 +0800

    optimize: replace/fadeIn

--- src/components/react-lazyimg.js
+++ src/components/react-lazyimg.js
@@ -8,24 +8,20 @@ import noop from 'noop';
 export default class extends React.PureComponent{
 
   static propTypes = {
-    lazy:PropTypes.string,
-    title:PropTypes.string,
+    lazy:PropTypes.bool,
     url:PropTypes.string,
     placeholder:PropTypes.string,
     effect:PropTypes.oneOf([
-      'replace', 'fade'
+      'replace',
+      'fade'
     ]),
     onLoad:PropTypes.func,
-    onClick:PropTypes.func,
-    style:PropTypes.object
   };
 
   static defaultProps = {
     lazy: false,
     onLoad:noop,
-    onClick:noop,
-    effect:'replace',
-    style:{}
+    effect:'replace'
   };
 
   constructor(props) {
@@ -42,81 +38,46 @@ export default class extends React.PureComponent{
   }
 
   componentWillReceiveProps(nextProps){
-    if(this.props.url!==nextProps.url){
+    if(this.props.url !== nextProps.url){
       this.initialState();
       this.setState(nextProps)
     }
-  }
 
-  get children(){
-    const { effect } = this.props;
-    switch(effect){
-      case 'replace':
-        return this.getReplaceChildren();
-      case 'fade':
-        return this.getFadeChildren();
+    if(this.props.lazy && !nextProps.lazy){
+      this.doShown();
     }
-    return null;
   }
 
-  getReplaceChildren(){
-    const {url,placeholder,title,className,effect,style,lazy,onClick} = this.props;
-    let target = placeholder || url;
-
-    return (
-      <img
-      ref='root'
-      data-effect={effect}
-      className={classNames('react-lazyimg',className)}
-      src={target} title={title}
-      style={style}
-      onClick={onClick}
-      onLoad={this._onReplace.bind(this)} />
-    );
-  }
-
-  getFadeChildren(){
-    const {title,effect,className,style,lazy,onClick} = this.props;
+  doShown(){
+    const dom = this.refs.root;
+    const {onLoad,effect} = this.props;
     const {url} = this.state;
-    return (
-      <img
-      ref='root'
-      data-effect={effect}
-      className={classNames('react-lazyimg',className)}
-      data-src={url}
-      src={url} title={title}
-      style={style}
-      onClick={onClick}
-      onLoad={this._onFade.bind(this)} />
-    );
+    this.setState({ loaded:true },()=>{
+      effect ==='replace' ? ( dom.src=url ): (dom.removeAttribute('data-src'));
+      onLoad.call(this);
+    });
   }
 
-  _onReplace(){
-    const {url,placeholder,onLoad} = this.props;
-    const dom = this.refs.root;
-    const { loaded } = this.state;
-    if(!loaded && placeholder){
-      this.setState({loaded:true},()=>{
-        dom.src=url;
-        onLoad.call(this);
-      });
+  _onLoad = inEvent => {
+    const {loaded} = this.state;
+    const {lazy} = this.props;
+    if(!loaded && !lazy){
+      this.doShown();
     }
-  }
-
-  _onFade(){
-    const {onLoad} = this.props;
-    const dom = this.refs.root;
-    const { loaded,url } = this.state;
-    if(!loaded){
-      this.setState({ loaded:true },()=>{
-        dom.removeAttribute('data-src');
-        onLoad.call(this);
-      });
-    }
-  }
+  };
 
 
   render(){
-    return this.children;
+    const {url,placeholder,className,effect,lazy,...props} = this.props;
+    const target = placeholder || url;
+    return (
+      <img
+        ref='root'
+        data-effect={effect}
+        className={classNames('react-lazyimg',className)}
+        data-src={url}
+        src={target}
+        onLoad={this._onLoad} />
+    );
   }
 }
--- src/dev.js
+++ src/dev.js
@@ -7,6 +7,7 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
+      lazy: true,
       test:'http://himg.bdimg.com/sys/portrait/item/be10475f686d6c723db0023244.jpg'
     };
   }
@@ -17,18 +18,27 @@ class App extends React.Component{
       });
     },2000)
   }
+
+  _click1 = e =>{
+    this.setState({lazy:false})
+  };
+
   render(){
     return (
       <div className="hello-react-lazyimg">
+
+        <button onClick={this._click1} >Click do Lazy</button>
         <h1>Replace</h1>
         <ReactLazyimg
         effect="replace"
+        lazy={this.state.lazy}
         placeholder="http://himg.bdimg.com/sys/portrait/item/be10475f686d6c723db0023244.jpg"
-        url="https://www.baidu.com/img/bd_logo1.png" title="Fei's logo" size="30px" />
+        url="https://cdn01.weipaitang.com/img/20170628C4wt5E-fO5OFcrmkWIXsapIVkC5GQGF6v8SynW7BRDVxitWPkDYw4M26FM3ZrHy3-W140H140" title="Fei's logo" size="30px" />
 
         <h1>Fade</h1>
         <ReactLazyimg
         effect="fade"
+        lazy={this.state.lazy}
         url='https://www.baidu.com/img/bd_logo1.png' title="Fei's logo" size="30px" />
       </div>
     );
