73430ebdd112937a91f63febf3f531f1f0d9a24e
afeiship <1290657123@qq.com>
2018-09-19 18:29:28 +0800

    issue: optmzie demo

--- src/dev.js
+++ src/dev.js
@@ -6,24 +6,44 @@ import ReactLazyImage from './main';
 // install: npm install afeiship/react-lazy-image --save
 // import : import ReactLazyImage from 'react-lazy-image'
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-
+    lazyValue: false
   };
 
-  constructor(props){
+  constructor(props) {
     super(props);
     window.demo = this;
     window.refs = this.refs;
     window.rc = this.refs.rc;
   }
 
-  render(){
+  _onClick1 = (inEvent) => {
+    this.setState({ lazyValue: true });
+  };
+
+  render() {
     // https://react-progressive-bg-image.netlify.com/static/media/image1.9c4f63a7.jpg
     // http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg
+    const { lazyValue } = this.state;
+
     return (
       <div className="hello-react-lazy-image">
+
+        <div className="list">
+          <div className="item">
             <ReactLazyImage src="http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg" ref='rc' />
+            <button disabled className="button">Auto load lazy Image </button>
+          </div>
+
+          <div className="item">
+            <ReactLazyImage
+              value={lazyValue}
+              lazy={true}
+              src="http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg" ref='rc' />
+            <button onClick={this._onClick1} className="button"> Trigger Lazy Image </button>
+          </div>
+        </div>
       </div>
     );
   }
@@ -31,6 +51,6 @@ class App extends React.Component{
 /*===example end===*/
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 );
