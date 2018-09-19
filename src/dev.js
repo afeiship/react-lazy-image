09ecbc77b50fdfe8a72398025fc6d477ef9d2e47
afeiship <1290657123@qq.com>
2018-09-19 22:08:29 +0800

    issue: add scroll load image demo

--- src/dev.js
+++ src/dev.js
@@ -18,7 +18,7 @@ function isElementInViewport(el, offset = 0) {
 class App extends React.Component {
   state = {
     lazyValue: true,
-    images:[
+    images: [
       'https://placeimg.com/640/480/nature',
       'https://placeimg.com/640/480/people',
       'https://placeimg.com/640/480/nature',
@@ -34,24 +34,38 @@ class App extends React.Component {
       'https://placeimg.com/640/480/nature',
       'https://placeimg.com/640/480/nature',
     ],
-    scrollValues: []
+    scrollLazyValues: []
   };
-
-  constructor(props) {
-    super(props);
-    window.demo = this;
-    window.refs = this.refs;
-    window.rc = this.refs.rc;
-  }
 
   _onClick1 = (inEvent) => {
     this.setState({ lazyValue: false });
   };
 
+  updateLazyValues() {
+    const { scrollLazyValues } = this.state;
+    const elements = document.querySelectorAll('.scroller .react-lazy-image');
+    const els = [].slice.call(elements);
+    els.forEach((el, index) => {
+      if (scrollLazyValues[index] === undefined || scrollLazyValues[index] === true) {
+        scrollLazyValues[index] = !isElementInViewport(el, 200);
+      }
+    });
+    console.log(scrollLazyValues);
+
+    this.setState({ scrollLazyValues });
+  }
+
+  componentDidMount() {
+    this.updateLazyValues();
+    window.onscroll = () => {
+      this.updateLazyValues();
+    }
+  }
+
   render() {
     // https://react-progressive-bg-image.netlify.com/static/media/image1.9c4f63a7.jpg
     // http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg
-    const { scrollValues, lazyValue } = this.state;
+    const { scrollLazyValues, lazyValue } = this.state;
 
     return (
       <div className="hello-react-lazy-image">
@@ -68,6 +82,21 @@ class App extends React.Component {
               src="http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg" ref='rc' />
             <button onClick={this._onClick1} className="button"> Trigger Lazy Image </button>
           </div>
+        </div>
+
+        <div className="scroller">
+          <center>Lazy load by scrolling</center>
+          {
+            this.state.images.map((item, index) => {
+              return (
+                <ReactLazyImage
+                  key={index}
+                  lazy={scrollLazyValues[index]}
+                  once={true}
+                  src="http://imglf5.nosdn.127.net/img/MmU4dzhkalUyS3ZaWXY3YzJxejdZNjF5c2t4UFRXTkwxRXNXbUNwdzFSYWpoM3NuSEZxbTF3PT0.jpg?imageView&thumbnail=2664y2000&type=jpg&quality=96&stripmeta=0&type=jpg" ref='rc' />
+              )
+            })
+          }
         </div>
       </div>
     );
